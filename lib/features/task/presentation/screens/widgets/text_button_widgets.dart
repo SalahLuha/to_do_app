7783890457583d854e814bf475b2a6ecfc4bd354
SalahLuha <salah223310@gmail.com>
2024-02-14 18:09:12 +0200

    App theme

--- lib/features/task/presentation/screens/widgets/text_button_widgets.dart
+++ lib/features/task/presentation/screens/widgets/text_button_widgets.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class TextButtonWidgets extends StatelessWidget {
   TextButtonWidgets({super.key, required this.text, this.onTap});
